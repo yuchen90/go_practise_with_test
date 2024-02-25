f8c2da8a8244e7ae7b7fa0265f09c0dcf5a69e08
WorkerYCH <houyc@yyits.cn>
2024-02-25 10:57:18 +0000

    HTTP Server Built

--- gameserver_test.go
+++ gameserver_test.go
@@ -1,24 +1,122 @@
-package gameserver_test
+package main
 
 import (
-	gameserver "go_practise/go_practise_with_test"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+type StubPlayerStore struct {
+	scores   map[string]int
+	winCalls []string
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	score := s.scores[name]
+	return score
+}
+
+func (s *StubPlayerStore) RecordWin(name string) {
+	s.winCalls = append(s.winCalls, name)
+}
+
 func TestGamerServer(t *testing.T) {
+	store := StubPlayerStore{
+		map[string]int{
+			"Pepper": 20,
+			"Floyd":  10,
+		},
+		[]string{},
+	}
+	server := &PlayerServer{&store}
+
 	t.Run("return Pepper's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/player/Pepper", nil)
+		request := newGetScoreRequest("Pepper")
 		response := httptest.NewRecorder()
 
-		gameserver.PlayerServer(request, response)
+		server.ServeHTTP(response, request)
 
-		got := response.Body.String()
-		want := "20"
+		assertHeaderStatus(t, response.Code, http.StatusOK)
+		assertResponseBody(t, response.Body.String(), "20")
+	})
+	t.Run("return Floyd's score", func(t *testing.T) {
+		request := newGetScoreRequest("Floyd")
+		response := httptest.NewRecorder()
 
-		if got != want {
-			t.Errorf("want %q, got %q", want, got)
+		server.ServeHTTP(response, request)
+
+		assertHeaderStatus(t, response.Code, http.StatusOK)
+		assertResponseBody(t, response.Body.String(), "10")
+	})
+	t.Run("return 404 on missing players", func(t *testing.T) {
+		request := newGetScoreRequest("Apollo")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		got := response.Code
+		want := http.StatusNotFound
+
+		assertHeaderStatus(t, got, want)
+	})
+}
+
+func TestStoreWins(t *testing.T) {
+	store := StubPlayerStore{
+		map[string]int{},
+		[]string{},
+	}
+	server := &PlayerServer{&store}
+
+	t.Run("it returns accepted on POST", func(t *testing.T) {
+		request := newPostWinRequest("Pepper")
+
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+		assertHeaderStatus(t, response.Code, http.StatusAccepted)
+		if len(store.winCalls) != 1 {
+			t.Errorf("got %d calls to RecordWin, want %d", len(store.winCalls), 1)
+		}
+		if store.winCalls[0] != "Pepper" {
+			t.Errorf("did not store correct winner, got %q, want %q", store.winCalls[0], "Pepper")
 		}
 	})
+}
+
+func TestLeague(t *testing.T) {
+	store := StubPlayerStore{}
+	server := &PlayerServer{&store}
+	t.Run("it returns 200 on /league", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/league", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertHeaderStatus(t, response.Code, http.StatusOK)
+	})
+}
+
+func newPostWinRequest(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodPost, "/players/"+name, nil)
+	return req
+}
+
+func newGetScoreRequest(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, "/players/"+name, nil)
+	return req
+}
+
+func assertResponseBody(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("want %q, got %q", want, got)
+	}
+}
+
+func assertHeaderStatus(t *testing.T, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("want Header status %d, got %d", want, got)
+	}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module httpgameserver
+
+go 1.21.6
